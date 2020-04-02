c603a8e58b133b06084b365fffab98a3db13e9e1
黄凯伟 <huangkaiwei@hengqijy.com>
2020-04-02 11:49:45 +0800

    GLFW测试新建窗口

--- OpenGLGLFW/OpenGLGLFW/main.cpp
+++ OpenGLGLFW/OpenGLGLFW/main.cpp
@@ -19,14 +19,21 @@ const GLuint WIDTH = 800, HEIGHT = 600;
 
 int main(int argc,char *argv[])
 {
-    
+    // 初始化GLFW
     glfwInit();
+    // 主版本号
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // 次版本号
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    // Create a GLFWwindow object that we can use for GLFW's functions
+    // mac配置需要这一行
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    // 创建窗口
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    
+    // 判断窗口是否创建成功
     if (window == nullptr)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -43,21 +50,22 @@ int main(int argc,char *argv[])
         return -1;
     }
 
-    // Define the viewport dimensions
+    // 定义窗口大小
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
 
-    // Game loop
+    // 开启循环绘制
     while (!glfwWindowShouldClose(window))
     {
-        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+        // 检查响应事件（鼠标、键盘输入）
         glfwPollEvents();
-        // Swap the screen buffers
+        
+        // S交换屏幕缓冲区
         glfwSwapBuffers(window);
     }
 
-    // Terminate GLFW, clearing any resources allocated by GLFW.
+    // 释放资源
     glfwTerminate();
     return 0;
 }
