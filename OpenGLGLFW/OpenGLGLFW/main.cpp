f6dd47e71e5aca1e489533c94632611f7d42e3f9
黄凯伟 <huangkaiwei@hengqijy.com>
2020-04-02 09:48:53 +0800

    GLFW测试

---
+++ OpenGLGLFW/OpenGLGLFW/main.cpp
@@ -0,0 +1,63 @@
+//
+//  main.cpp
+//  OpenGLGLFW
+//
+//  Created by huangkaiwei on 2020/4/2.
+//  Copyright © 2020 hq_kven. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+// GLEW
+#define GLEW_STATIC
+#include "glew.h"
+#include "glfw3.h"
+#include "glfw3native.h"
+
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+int main(int argc,char *argv[])
+{
+    
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    // Create a GLFWwindow object that we can use for GLFW's functions
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+    glewExperimental = GL_TRUE;
+    // Initialize GLEW to setup the OpenGL Function pointers
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    // Define the viewport dimensions
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    // Game loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+        glfwPollEvents();
+        // Swap the screen buffers
+        glfwSwapBuffers(window);
+    }
+
+    // Terminate GLFW, clearing any resources allocated by GLFW.
+    glfwTerminate();
+    return 0;
+}
