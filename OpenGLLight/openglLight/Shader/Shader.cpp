66ad7c292c52b405e594cc7adc04f1303fa4c035
黄凯伟 <huangkaiwei@hengqijy.com>
2020-04-10 15:39:51 +0800

    光照

---
+++ OpenGLLight/openglLight/Shader/Shader.cpp
@@ -0,0 +1,112 @@
+//
+//  shader.cpp
+//  OpenGLGLFW
+//
+//  Created by kven on 2020/4/6.
+//  Copyright © 2020 hq_kven. All rights reserved.
+//
+
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include "glew.h"
+
+class Shader
+{
+public:
+    GLuint Program;
+    // 通过本地文件读取着色器代码，并关联着色器程序
+    Shader(const GLchar* vertexPath, const GLchar* fragmentPath)
+    {
+        // 着色器代码和文件
+        std::string vertexCode;
+        std::string fragmentCode;
+        std::ifstream vShaderFile;
+        std::ifstream fShaderFile;
+        // 确保ifstream对象可以引发异常：
+        vShaderFile.exceptions (std::ifstream::badbit);
+        fShaderFile.exceptions (std::ifstream::badbit);
+        try
+        {
+            // 打开着色器文件
+            vShaderFile.open(vertexPath);
+            fShaderFile.open(fragmentPath);
+            std::stringstream vShaderStream, fShaderStream;
+            //读取文件内容缓存至 streams
+            vShaderStream << vShaderFile.rdbuf();
+            fShaderStream << fShaderFile.rdbuf();
+            // 关闭文件
+            vShaderFile.close();
+            fShaderFile.close();
+            // 将流转换为字符串
+            vertexCode = vShaderStream.str();
+            fragmentCode = fShaderStream.str();
+        }
+        catch (std::ifstream::failure e)
+        {
+            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+        }
+        // 顶点着色器c字符串
+        const GLchar* vShaderCode = vertexCode.c_str();
+        
+        // 片段着色器c字符串
+        const GLchar * fShaderCode = fragmentCode.c_str();
+        // 生命顶点、片段着色器
+        GLuint vertex, fragment;
+        // 着色器编译连接信息
+        GLint success;
+        GLchar infoLog[512];
+        // 顶点着色器
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        // 连接着色器代码资源
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        // 编译连接着色器代码
+        glCompileShader(vertex);
+        // 判断着色器程序是否连接成功
+        glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+        
+        // 片着色器
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+        glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+        
+        // 着色器程序
+        this->Program = glCreateProgram();
+        glAttachShader(this->Program, vertex);
+        glAttachShader(this->Program, fragment);
+        glLinkProgram(this->Program);
+        glGetProgramiv(this->Program, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            glGetProgramInfoLog(this->Program, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        }
+        // 删除着色器，因为它们已链接到着色器程序中了
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+
+    }
+    // 激活着色器程序
+    void Use()
+    {
+        glUseProgram(this->Program);
+    }
+};
+
+#endif
---
+++ OpenGLLight/openglLight/main.cpp
@@ -0,0 +1,277 @@
+//
+//  main.cpp
+//  OpenGLLight
+//
+//  Created by huangkaiwei on 2020/4/10.
+//  Copyright © 2020 hq_kven. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include <iostream>
+// GLEW
+#define GLEW_STATIC
+#include "glew.h"
+#include "glfw3.h"
+#include "glfw3native.h"
+
+#include "Shader/Shader.cpp"
+#include "Camera/Camera.cpp"
+
+// glm
+#include "glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+// 申明键盘输入回调
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+// 根据按下的按键来更新摄像机的值
+void do_movement();
+// 鼠标事件监听
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+// 鼠标滚动监听
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+bool firstMouse = true;
+
+// 摄像机
+Camera  camera(glm::vec3(0.0f, 0.0f, 3.0f));
+GLfloat lastX  =  WIDTH  / 2.0;
+GLfloat lastY  =  HEIGHT / 2.0;
+bool    keys[1024];
+
+// 光源位置
+glm::vec3 lightPos(0.8f, 1.0f, 2.0f);
+
+// 每一帧绘制事件
+GLfloat deltaTime = 0.0f;   
+GLfloat lastFrame = 0.0f;
+
+int main(int argc,char *argv[]) {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // 次版本号
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // 窗口尺寸不可变
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    // mac配置需要这一行
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    // 创建窗口
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    
+    // 判断窗口是否创建成功
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    /// 事件绑定
+    
+    // 绑定键盘事件
+    glfwSetKeyCallback(window, key_callback);
+    // 设置捕获鼠标位置并因此光标
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    // 添加鼠标监听
+    glfwSetCursorPosCallback(window, mouse_callback);
+    // 监听鼠标滚动
+    glfwSetScrollCallback(window, scroll_callback);
+    
+    // 使用现代方法来检索函数指针和扩展
+    glewExperimental = GL_TRUE;
+    // 初始化GLEW以设置OpenGL函数指针
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    // 定义窗口大小
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    // 设置视口大小，与窗口一样大
+    glViewport(0, 0, width, height);
+    //开启深度测试
+    glEnable(GL_DEPTH_TEST);
+    
+    Shader ourShader("shader.vs", "shader.frag");
+    Shader lightShader("lightShader.vs","lightShader.frag");
+    
+    // 正方形顶点
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+
+        -0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f, -0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f,  0.5f,
+        -0.5f, -0.5f, -0.5f,
+
+        -0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+         0.5f,  0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f, -0.5f
+    };
+    
+    GLuint VBO,VAO;
+    glGenVertexArrays(1,&VAO);
+    glGenBuffers(1,&VBO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+    
+    GLuint lightVAO;
+    glGenVertexArrays(1, &lightVAO);
+    glBindVertexArray(lightVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+    
+    
+    // 开启循环绘制
+    while (!glfwWindowShouldClose(window))
+    {
+        
+        GLfloat currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        glfwPollEvents();
+        do_movement();
+
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        ourShader.Use();
+        GLint objectColorLoc = glGetUniformLocation(ourShader.Program, "objectColor");
+        GLint lightColorLoc  = glGetUniformLocation(ourShader.Program, "lightColor");
+        glUniform3f(objectColorLoc, 1.0f, 0.5f, 0.31f);
+        glUniform3f(lightColorLoc,  1.0f, 0.5f, 1.0f);
+
+        glm::mat4 view;
+        view = camera.GetViewMatrix();
+        glm::mat4 projection = glm::perspective(camera.Zoom, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+        GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
+        GLint viewLoc  = glGetUniformLocation(ourShader.Program,  "view");
+        GLint projLoc  = glGetUniformLocation(ourShader.Program,  "projection");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        glBindVertexArray(VAO);
+        glm::mat4 model;
+        model = glm::rotate(model, 5.0f, glm::vec3(1.0f, 0.3f, 0.5f));
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+
+        lightShader.Use();
+        modelLoc = glGetUniformLocation(lightShader.Program, "model");
+        viewLoc  = glGetUniformLocation(lightShader.Program, "view");
+        projLoc  = glGetUniformLocation(lightShader.Program, "projection");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+        model = glm::mat4();
+        model = glm::translate(model, lightPos);
+        model = glm::scale(model, glm::vec3(0.2f));
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        
+        glBindVertexArray(lightVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(window);
+    }
+    
+    // 释放资源
+    glfwTerminate();
+    return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    // 当用户按下ESC键,我们设置window窗口的WindowShouldClose属性为true
+    // 关闭应用程序
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    // 按钮按下、松手记录状态
+    if(action == GLFW_PRESS)
+        keys[key] = true;
+    else if(action == GLFW_RELEASE)
+        keys[key] = false;
+}
+
+void do_movement()
+{
+  // 摄像机控制
+  if(keys[GLFW_KEY_W])
+      camera.ProcessKeyboard(FORWARD, deltaTime);
+  if(keys[GLFW_KEY_S])
+      camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if(keys[GLFW_KEY_A])
+        camera.ProcessKeyboard(LEFT, deltaTime);
+  if(keys[GLFW_KEY_D])
+      camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    // 判断是否为首次出现鼠标位置，避免因初始位置偏移量过大产生跳动
+    if(firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+    // 鼠标偏移量
+    GLfloat xoffset = xpos - lastX;
+    GLfloat yoffset = lastY - ypos;
+    // 记录最后一帧鼠标偏移量
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(yoffset);
+}
